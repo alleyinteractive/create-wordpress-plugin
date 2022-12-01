6773eb0948bab4b7a3d5e9521fb3474a15c3d239
Steven Slack <stevenslack@gmail.com>
2022-12-01 17:09:15 -0500

    correct path for phpcs exclusion, update comment

--- entries/example-entry/index.js
+++ entries/example-entry/index.js
@@ -2,5 +2,5 @@
  * Example entry point to enqueue.
  *
  * Entries are automatically built with webpack when included inside the
- * ../entries directory. Enqueue them in inc/assets.php
+ * ../entries directory. Enqueue in an entry index.php file or in src/assets.php
  */
