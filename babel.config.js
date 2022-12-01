ab50a038dccb79a5be7eccaa8706927594292e80
Steven Slack <stevenslack@gmail.com>
2022-12-01 14:27:42 -0500

    fix jest to work with Typescript

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/**
+ * This babel config is used by Jest to ensure that tests
+ * will run correctly in Typescript and JavaScript files.
+ *
+ * Jest supports TypeScript, via Babel.
+ *
+ * @see https://jestjs.io/docs/getting-started#using-typescript
+ */
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+  ],
+};
