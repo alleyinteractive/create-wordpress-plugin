84f49ffc5e0ac3af192ee2de3e0d67abdfe79e42
Sean Fisher <srtfisher@gmail.com>
2022-12-12 15:19:50 -0500

    Revert "Remove all references to the alley-scaffolder"
    This reverts commit 8125983934524cad15f73627bbf072a6605a40e1.

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom post type and taxonomy registration.
+ *
+ * To be used with the @alleyco/scaffold NPM package.
+ *
+ * @package create-wordpress-plugin
+ */
+
+namespace Create_WordPress_Plugin;
+
+// Content types and taxonomies should be included below. In order to scaffold
+// them, leave the Begin and End comments in place.
+/* Begin Data Structures */
+
+/* End Data Structures */
--- plugin.php
+++ plugin.php
@@ -48,6 +48,7 @@ if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Load the plugin's main files.
+require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/src/assets.php';
 require_once __DIR__ . '/src/meta.php';
 
