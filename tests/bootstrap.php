116e1efc31cbbb8c9030f7078f0d89f223a042c6
Sean Fisher <srtfisher@gmail.com>
2023-11-15 11:10:47 -0500

    Disable sqlite by default

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,8 @@
 /**
  * Create WordPress Plugin Tests: Bootstrap
  *
+ * phpcs:disable Squiz.Commenting.InlineComment.InvalidEndChar
+ *
  * @package create-wordpress-plugin
  */
 
@@ -9,8 +11,12 @@
  * Visit {@see https://mantle.alley.com/testing/test-framework.html} to learn more.
  */
 \Mantle\Testing\manager()
+	// Rsync the plugin to plugins/create-wordpress-plugin when testing.
 	->maybe_rsync_plugin()
-	->with_sqlite()
+
+	// Use SQLite for testing instead of SQL (disabled by default).
+	// ->with_sqlite()
+
 	// Load the main file of the plugin.
 	->loaded( fn () => require_once __DIR__ . '/../plugin.php' )
 	->install();
