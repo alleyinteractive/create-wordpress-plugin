1af388be816112b019fe976077bf3c1ff8a13acd
Sean Fisher <srtfisher@gmail.com>
2025-10-23 14:35:54 -0400

    Add caching to load entries

--- src/features/class-load-entries.php
+++ src/features/class-load-entries.php
@@ -20,19 +20,50 @@ use function Alley\WP\Create_WordPress_Plugin\validate_path;
  */
 class Load_Entries implements Feature {
 	/**
+	 * Constructor.
+	 *
+	 * @param bool $cache Whether to use APCu caching for entry files. Default true.
+	 */
+	public function __construct( public readonly bool $cache = false ) {}
+
+	/**
 	 * Boot the feature.
 	 */
 	public function boot(): void {
-		$files = glob( CREATE_WORDPRESS_PLUGIN_DIR . '/build/**/index.php' );
-
-		if ( ! is_array( $files ) ) {
-			return;
-		}
-
-		foreach ( $files as $path ) {
+		foreach ( $this->get_entry_files() as $path ) {
 			if ( validate_path( $path ) ) {
 				require_once $path;  // phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.IncludingFile, WordPressVIPMinimum.Files.IncludingFile.UsingVariable
 			}
 		}
+	}
+
+	/**
+	 * Get the list of entry files.
+	 *
+	 * Uses APCu caching if available.
+	 *
+	 * @return string[] List of entry file paths.
+	 */
+	protected function get_entry_files(): array {
+		if ( $this->cache && function_exists( 'apcu_fetch' ) ) {
+			/**
+			 * Retrieve the entries from the APCu cache.
+			 *
+			 * @var string[]|false
+			 */
+			$cache = apcu_fetch( 'create_wordpress_plugin_entries' );
+
+			if ( is_array( $cache ) ) {
+				return $cache;
+			}
+		}
+
+		$files = glob( CREATE_WORDPRESS_PLUGIN_DIR . '/build/**/index.php' ) ?: [];
+
+		if ( $this->cache && function_exists( 'apcu_store' ) ) {
+			apcu_store( 'create_wordpress_plugin_entries', $files, HOUR_IN_SECONDS );
+		}
+
+		return $files;
 	}
 }
--- src/main.php
+++ src/main.php
@@ -13,11 +13,22 @@ use Alley\WP\Features\Group;
  * Instantiate the plugin.
  */
 function main(): void {
-	// Add features here.
 	$plugin = new Group(
 		new Features\Register_Block_Manifest(),
-		new Features\Load_Entries(),
+		new Features\Load_Entries( cache: 'local' !== wp_get_environment_type() ),
 	);
+
+	/*
+	 * Add additional features here.
+	 *
+	 * Example:
+	 *
+	 *   $plugin->include( new Features\My_New_Feature() );
+	 *
+	 * You can generate a new feature using `npx @alleyinteractive/scaffolder@latest feature`.
+	 *
+	 * @see https://github.com/alleyinteractive/wp-type-extensions
+	 */
 
 	$plugin->boot();
 }
