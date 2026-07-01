219eabb21c8e2efd1584e530c9f41320b8d62a51
mboynes <mboynes@users.noreply.github.com>
2026-07-01 07:24:07 +0000

    Upgrade plugin to 7.0

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@
  * Author URI: https://github.com/alleyinteractive/create-wordpress-plugin
  * Requires at least: 6.5
  * Requires PHP: 8.2
- * Tested up to: 6.8
+ * Tested up to: 7.0
  *
  * Text Domain: create-wordpress-plugin
  * Domain Path: /languages/
